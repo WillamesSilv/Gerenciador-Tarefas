b0902f210ed824eda08687bfa98a7337e78d22b0
Willames Santos <willamessantos@live.com>
2021-09-13 10:44:14 -0300

    finished project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
-import { v4 as uuidv4 } from 'uuid'
+import Header from './components/Header'
+import TaskDetails from './components/TaskDetails'
+
 import './App.css'
+import axios from 'axios'
 
 const App = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: 'Estudar Programação',
-      completed: false
-    },
-    {
-      id: 2,
-      title: 'Ler livros',
-      completed: true
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const fetchTask = async () => {
+      const { data } = await axios.get(
+        'https://jsonplaceholder.cypress.io/todos?_limit=5'
+      )
+      setTasks(data)
     }
-  ])
+    fetchTask()
+  }, [])
 
   const handleTaskClick = taskId => {
     const newTasks = tasks.map(task => {
@@ -38,11 +43,32 @@ const App = () => {
     setTasks(newTasks)
   }
 
+  const handleTaskDeletion = taskId => {
+    const newTasks = tasks.filter(task => task.id !== taskId)
+    setTasks(newTasks)
+  }
+
   return (
-    <div className="container">
-      <AddTask handleTaskAddition={handleTaskAddition} />
-      <Tasks tasks={tasks} handleTaskClick={handleTaskClick} />
-    </div>
+    <Router>
+      <div className="container">
+        <Header />
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <>
+              <AddTask handleTaskAddition={handleTaskAddition} />
+              <Tasks
+                tasks={tasks}
+                handleTaskClick={handleTaskClick}
+                handleTaskDeletion={handleTaskDeletion}
+              />
+            </>
+          )}
+        />
+        <Route path="/:taskTitle" exact component={TaskDetails} />
+      </div>
+    </Router>
   )
 }
 
